c555418452638900fc909ae0184efb9b4f1da65d
Deep Toaster <yizhenwilliam@gmail.com>
2017-04-03 18:11:05 -0700

    updated font to add Couchsurfing

--- index.php
+++ index.php
@@ -26,6 +26,8 @@
     <div id="about" class="section">
       <div class="map map-left">
         <img src="/lib/img/asia.png" alt="" />
+        <span style="width: 2.5em; height: 2em; top: 9.3em; left: 33.8em; -webkit-transform: rotate(-37deg); -o-transform: rotate(-37deg); -moz-transform: rotate(-37deg); transform: rotate(-37deg);"></span>
+        <span style="width: 2.5em; height: 2em; top: 9.3em; left: 33.8em; -webkit-transform: rotate(143deg); -o-transform: rotate(143deg); -moz-transform: rotate(143deg); transform: rotate(143deg);"></span>
       </div>
       <div class="map map-right">
         <img src="/lib/img/north_america.png" alt="" />
@@ -54,6 +56,7 @@
           <span class="reel">
             <span class="reel-symbol">Game</span>
             <span class="reel-symbol">Graphic</span>
+            <span class="reel-symbol">Industrial</span>
             <span class="reel-symbol">Web</span>
           </span>
           <span>designer, avid <em>hitchhiker</em>, and student of life who drinks tea and poops code.</span>
@@ -87,15 +90,15 @@
             <h3>Presence</h3>
             <ul id="presence">
               <li>
-                <i class="flaticon-mail-envelope"></i>
+                <i class="flaticon-note"></i>
                 <a href="mailto:yizhenwilliam@gmail.com">Email</a>
               </li>
               <li>
-                <i class="flaticon-facebook-app-logo"></i>
+                <i class="flaticon-square"></i>
                 <a href="https://www.facebook.com/yizhenwilliam">Facebook</a>
               </li>
               <li>
-                <i class="flaticon-people"></i>
+                <i class="flaticon-couchsurfing-logo"></i>
                 <a href="https://www.couchsurfing.com/people/yizhenwilliam">Couchsurfing</a>
               </li>
               <li>
@@ -107,9 +110,39 @@
         </div>
       </div>
     </div>
-    <div id="activities" class="section">
+    <div id="interests" class="section">
       <div class="content">
-        <h2>Activities</h2>
+        <h2>Interests</h2>
+        <p class="lead">Things I'm learning, mostly.</p>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span>building things</span>
+          </li>
+          <li class="list-group-item">
+            <span>calligraphy</span>
+          </li>
+          <li class="list-group-item">
+            <span>climbing</span>
+          </li>
+          <li class="list-group-item">
+            <span>fencing</span>
+          </li>
+          <li class="list-group-item">
+            <span>fire spinning</span>
+          </li>
+          <li class="list-group-item">
+            <span>making music</span>
+          </li>
+          <li class="list-group-item">
+            <span>self-defense</span>
+          </li>
+          <li class="list-group-item">
+            <span>urban exploration</span>
+          </li>
+          <li class="list-group-item">
+            <span>writing</span>
+          </li>
+        </ul>
       </div>
       <div class="coaster">
         <div class="coaster-track" style="margin-left: -64.5em">
@@ -155,7 +188,7 @@
           <span class="coaster-line" style="bottom: 7.3em; left: 33.5em; width: 3em; -webkit-transform: rotate(60deg); -o-transform: rotate(60deg); -moz-transform: rotate(60deg); transform: rotate(60deg);"><span></span></span>
           <span class="coaster-cup" style="bottom: 2.7em; left: 35.1em; width: 5em; height: 8em;"><span></span></span>
           <span class="coaster-line" style="bottom: 4.5em; left: 40.2em; width: 6em; -webkit-transform: rotate(-60deg); -o-transform: rotate(-60deg); -moz-transform: rotate(-60deg); transform: rotate(-60deg);"><span></span></span>
-          <span class="coaster-clear" style="bottom: 9.8em; left: 5em; width: 38em; height: 2.2em;"></span>
+          <span class="coaster-clear" style="bottom: 9.7em; left: 5em; width: 38em; height: 2.3em;"></span>
           <span class="coaster-clear" style="bottom: 6.6em; left: 15em; width: 6em; height: 3.2em;"></span>
           <span class="coaster-clear" style="bottom: 7.5em; left: 21em; width: 5.6em; height: 2.3em;"></span>
           <span class="coaster-clear" style="bottom: 7.5em; left: 33.6em; width: 7em; height: 2.3em;"></span>
