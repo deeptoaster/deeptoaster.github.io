2a8b5e3d42c0e2905f311617984928680ef63523
Deep Toaster <deeptoaster@gmail.com>
2017-11-29 01:27:50 -0800

    updated Cleverly #i1oWnc48

--- src/index.php
+++ src/index.php
@@ -1,25 +1,12 @@
 <?
+if (PHP_SAPI != 'CLI') {
+  header('Location: /', true, 301);
+}
+
 include(__DIR__ . '/../lib/cleverly/Cleverly.class.php');
 
 $cleverly = new Cleverly();
-$cleverly->preserve_indent = true;
+$cleverly->preserveIndent = true;
 $cleverly->setTemplateDir(__DIR__ . '/templates');
-
-$cleverly->display('index.tpl', array(
-  'about' => function() {
-    global $cleverly;
-
-    $cleverly->display('about.tpl');
-  },
-  'interests' => function() {
-    global $cleverly;
-
-    $cleverly->display('interests.tpl');
-  },
-  'gallery' => function() {
-    global $cleverly;
-
-    $cleverly->display('gallery.tpl');
-  }
-));
+$cleverly->display('index.tpl');
 ?>
